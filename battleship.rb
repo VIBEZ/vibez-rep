8c15445e438daf19a62c11bdfdb6aa894a86af13
Michael <miobodunrin1@sheffield.ac.uk>
2014-12-17 19:16:42 +0000

    First Commit

--- battleship.rb
+++ battleship.rb
@@ -35,47 +35,6 @@ class Board
     @grid = Array.new(10).map! { Array.new(10, char) }
   end
 
-  def place_ship(ship, start_position, orientation)
-    puts "i'm gonna place a ship"
-    row = start_position[:row]
-    column = start_position[:column]
-    ship.length.times do
-      if orientation == :horizontal
-        self.grid[row][column].ship = ship
-        self.grid[row][column].status = :occupied
-        column += 1
-      else
-        self.grid[row][column].ship = ship
-        self.grid[row][column].status = :occupied
-        row += 1
-      end
-    end
-  end
-
-  #validate ship placement remains within board
-  def valid_coordinates?(ship, start_position, orientation)
-    if orientation == :horizontal
-      (start_position[:column] + ship.length) <= 10
-    else
-      (start_position[:row] + ship.length) <= 10
-    end
-  end
-
-  #validate ship placement conflicts with another placed ship
-  def check_clearance?(ship, start_position, orientation)
-    row = start_position[:row]
-    column = start_position[:column]
-    ship.length.times do
-      if self.grid[row][column].ship
-        return false
-      elsif orientation == :horizontal
-        column += 1
-      else
-        row += 1
-      end
-    end
-    return true
-  end
 
 
 
@@ -131,15 +90,15 @@ class Board
 
     #print side-by-side ship status and shots taken boards
     def print_boards
-      puts "      SHIP STATUS                   SHOTS TAKEN".colorize(:light_red)
-      puts "  1 2 3 4 5 6 7 8 9 10          1 2 3 4 5 6 7 8 9 10".colorize(:green)
+      puts "      SHIP STATUS                   SHOTS TAKEN".colorize(:black)
+      puts "  1 2 3 4 5 6 7 8 9 10          1 2 3 4 5 6 7 8 9 10".colorize(:black)
       row_letter = ('A'..'Z').to_a
       row_number = 0
       @board.grid.each do |row1|
-        print row_letter[row_number].colorize(:green) + ' '
+        print row_letter[row_number].colorize(:black) + ' '
         row1.each {|cell| print cell.to_s + ' '}
         print "        "
-        print row_letter[row_number].colorize(:green) + ' '
+        print row_letter[row_number].colorize(:black) + ' '
         @target_board.grid[row_number].each {|cell| print cell.to_s + ' '}
         print "\n"
         row_number += 1
@@ -225,12 +184,12 @@ class Board
 
 
   def play
-    print "Wanna play some Battleship? (Y/N)".colorize(:light_green)
+    print "Wanna play some Battleship? (Y/N)".colorize(:black)
     response = gets.chomp.strip.upcase
 
     unless response == "Y" or response == "N"
       puts "Invalid response, Try Again"
-      print "Wanna play some Battleship? (Y/N) ".colorize(:light_green)
+      print "Wanna play some Battleship? (Y/N) ".colorize(:black)
       response = gets.chomp.strip.upcase
     end
     puts response
